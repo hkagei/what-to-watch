f23745779467db9f2aab04b633924f2c2462a591
hkagei <k_dawg_1022@hotmail.com>
2022-08-07 19:22:40 -0600

    inital commit for final project

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,54 @@
+// Type definitions, or TypeDefs for short, involves literally defining every piece of data that the client can expect to work with through a query or mutation. Every GraphQL API starts with defining this data, as this type of strict type definition will give the client more clarity as to what they are asking for and what they can expect in return. Think of this as not only defining the API endpoint, but also defining the exact data and parameters that are tied to that endpoint.
+
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    # _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): Auth
+    removeBook(bookId: ID!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
