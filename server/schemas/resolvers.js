150de559f9decc74f765988228b362c72a7a6e74
clarkspencerc <clarkspencerc@gmail.com>
2022-08-13 18:21:24 -0600

    auth is working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -57,7 +57,7 @@ const resolvers = {
             //   }
             
               // throw new AuthenticationError('You need to be logged in!');
-            },
+            //},
         //     addReaction: async (parent, { thoughtId, reactionBody }, context) => {
         //       if (context.user) {
         //         const updatedThought = await Thought.findOneAndUpdate(
@@ -84,23 +84,23 @@ const resolvers = {
             
         //       throw new AuthenticationError('You need to be logged in!');
         //     },
-        //     login: async (parent, { email, password }) => {
-        //       const user = await User.findOne({ email });
+            login: async (parent, { email, password }) => {
+              const user = await User.findOne({ email });
             
-        //       if (!user) {
-        //         throw new AuthenticationError('Incorrect credentials');
-        //       }
+              if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+              }
             
-        //       const correctPw = await user.isCorrectPassword(password);
+              const correctPw = await user.isCorrectPassword(password);
             
-        //       if (!correctPw) {
-        //         throw new AuthenticationError('Incorrect credentials');
-        //       }
+              if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+              }
             
-        //       const token = signToken(user);
-        //       return { token, user };
-        //     }
-        // }
+              const token = signToken(user);
+              return { token, user };
+            }
+          }
 };
 
 module.exports = resolvers;
