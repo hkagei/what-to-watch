4b9b84cf0315ae6d91ffbb840cb74cfb1442d83f
TessShearer <shearer.tess@gmail.com>
2022-08-15 17:28:04 -0600

    begin editing saved movies

--- client/src/pages/SavedMovies.js
+++ client/src/pages/SavedMovies.js
@@ -1,40 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { REMOVE_MOVIE } from '../utils/mutations';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteMovie } from '../utils/API';
+import { QUERY_ME } from '../utils/queries';
+import { deleteMovie } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeMovieId } from '../utils/localStorage';
 
 const SavedMovies = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(QUERY_ME);
+  const userData = data?.me || {};
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  // const [removeMovie, { error }] = useMutation(REMOVE_MOVIE);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // const userDataLength = Object.keys(userData).length;
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-          return false;
-        }
+  //       if (!token) {
+  //         console.log('no token')
+  //         return false;
+  //       }
 
-        const response = await getMe(token);
+  //       const response = await getMe(token);
+  //       console.log(response)
+  //       if (!response.ok) {
+  //         throw new Error('something went wrong!');
+  //       }
 
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
+  //       const user = await response.json();
+  //       setUserData(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+  //   getUserData();
+  // }, [userDataLength]);
 
   // create function that accepts the Movie's mongo _id value as param and deletes the Movie from the database
   const handleDeleteMovie = async (movieId) => {
@@ -45,25 +49,23 @@ const SavedMovies = () => {
     }
 
     try {
-      const response = await deleteMovie(movieId, token);
+      const { data } = await deleteMovie(movieId, token);
+      removeMovieId(movieId);
 
-      if (!response.ok) {
+      if (!data.ok) {
         throw new Error('something went wrong!');
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove movie's id from localStorage
-      removeMovieId(movieId);
+      // const updatedUser = await response.json();
+      // setUserData(updatedUser);
+      // // upon success, remove movie's id from localStorage
+      // removeMovieId(movieId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
+  if (loading) return 'Loading...';
 
   return (
     <>
