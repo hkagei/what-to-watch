e2fcda32f2491e4e8f5176d0914f75e89353ad0f
hkagei <k_dawg_1022@hotmail.com>
2022-08-15 19:21:48 -0600

    update code, fix saveMovie line

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,7 +6,7 @@ export const QUERY_ME = gql` {
         username
         email
         savedMovie{
-            movieID
+            movieId
             original_title
             overview
             poster_path
