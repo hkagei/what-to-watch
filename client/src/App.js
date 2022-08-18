0fa735ae2c077457c6935c04ffc4c21101928246
jaysmith13 <janeasmith13@gmail.com>
2022-08-17 18:40:37 -0600

    stripe payment update
    css and payment update

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,17 @@ import SearchMovies from './pages/SearchMovies';
 import SavedMovies from './pages/SavedMovies';
 import Navbar from './components/Navbar';
 
+//Stripe imports
+import{
+  Elements,
+  CardElement,
+  useStripePromise,
+  useElements,
+} from "@stripe/react-stripe-js";
+  import {retrieveStripe} from '@stripe/stripe-js';
+  import "./App.css";
+  
+
 import ApolloClient from 'apollo-boost';
 import { setContext } from '@apollo/client/link/context';
 import { ApolloProvider } from '@apollo/react-hooks';
@@ -38,5 +49,101 @@ function App() {
     </ApolloProvider>
   );
 }
+
+// Stripe functions
+
+const App = () => {
+  const stripePromise = retrieveStripe(
+    "pk_test_51LXFykLV2oOJPgrv1Gzdsp4JUD7d54Q16U4Ba2whg8vIvBcC6apN8sQaU0udrKOi0ZGSXD4ybcaMzSFNbA82RXQA00BiUxeTev"
+  );
+  return (
+    <Elements stripe={stripePromise}>
+      <CheckoutForm/>
+    </Elements>
+  );
+};
+function CheckoutForm() {
+  const [isPaymentLoading, setPaymentLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  const payMoney = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    setPaymentLoading(true);
+    const clientSecret = getClientSecret();
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "",
+        },
+      },
+    });
+    setPaymentLoading(false);
+    if (paymentResult.error) {
+      alert(paymentResult.error.message);
+    } else {
+      if (paymentResult.paymentIntent.status === "successful") {
+        alert("Your payment was successfully processed, thank you!");
+      }
+    }
+  };
+
+  return (
+    <div
+      style={{
+        padding: "3rem",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "500px",
+          margin: "0 auto",
+        }}
+      >
+        <form
+          style={{
+            display: "block",
+            width: "100%",
+          }}
+          onSubmit = {payMoney}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <CardElement
+              className="card"
+              options={{
+                style: {
+                  base: {
+                    backgroundColor: "gold"
+                  } 
+                },
+              }}
+            />
+            <button
+              className="pay-button"
+              disabled={isPaymentLoading}
+            >
+              {isPaymentLoading ? "Loading..." : "Pay"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
 
 export default App;
