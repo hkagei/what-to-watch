c143b01d45e1342196fe164161649627a7f4af1c
Cody Eddie <buffbuck0427@gmail.com>
2022-08-20 14:43:35 -0600

    revert

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
+<<<<<<< HEAD
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/what-to-watch', {
+=======
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+>>>>>>> parent of 838e5a14 ( updated mongo connection)
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
