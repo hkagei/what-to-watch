2e0fc56f9ce5ffdc35e376b5904242c179835ff9
clarkspencerc <clarkspencerc@gmail.com>
2022-08-20 16:19:33 -0600

    mongo trouble2

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/what-to-w
 
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
