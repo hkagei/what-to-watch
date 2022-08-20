2e2200e567771be9cc8a0a11f9b73962212832eb
clarkspencerc <clarkspencerc@gmail.com>
2022-08-20 17:20:06 -0600

    plz work 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
+const connection = "mongodb+srv://spencercclark:keepinitphresh123@cluster0.8ljzpth.mongodb.net/?retryWrites=true&w=majorityretryWrites=true&w=majorityretryWrites=true&w=majorityretryWrites=true&w=majority";
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plzwurk', {
+mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(() => console.log("Database Connected Successfully"))
+  .catch(err => console.log(err));
 
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // useCreateIndex: true,
-  //plzwurk is the name of the database
-  // useFindAndModify: false,
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plzwurk', {
+
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   // useCreateIndex: true,
+//   //plzwurk is the name of the database
+//   // useFindAndModify: false,
+// });
 
 module.exports = mongoose.connection;
