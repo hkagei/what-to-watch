5d200ee07def2f8a2fc88b9ae1115632b71e32b5
clarkspencerc <clarkspencerc@gmail.com>
2022-08-20 14:48:48 -0600

    mongo lowercase

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/what-to-watch', {
-=======
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
->>>>>>> parent of 838e5a14 ( updated mongo connection)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/what-to-watch'{
+
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
