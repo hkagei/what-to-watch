6a94e1205b6dc7721bcddf92e4c14b1174b84bb5
clarkspencerc <clarkspencerc@gmail.com>
2022-08-20 16:35:57 -0600

    database

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plzwurk',
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
+  //plzwurk is the name of the database
   // useFindAndModify: false,
 });
 
