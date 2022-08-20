540352e96117c5a3f3683be4555a175eac27a4ef
Cody Eddie <buffbuck0427@gmail.com>
2022-08-20 17:02:19 -0600

    change connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/what-to-watch', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/what-to-watch', {
 
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
+const connection = (process.env.MONGODB_URI || "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority");
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
 
 module.exports = mongoose.connection;
