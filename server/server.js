54632860b65a89a7754dd4fc432c5c28009d4d2d
jaysmith13 <janeasmith13@gmail.com>
2022-08-28 00:37:30 -0600

    Update server.js
    added .env

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-
+require("dotenv").config();
+console.log(process.env)
 const { ApolloServer } = require("apollo-server-express");
 
 const { authMiddleware } = require("./utils/auth");
